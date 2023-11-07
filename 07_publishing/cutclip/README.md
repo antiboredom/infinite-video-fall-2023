7e2ac054c903e78119f0a55bce6efd8288f13e17
Sam Lavigne <splavigne@gmail.com>
2023-11-07 17:41:41 -0500

    add poetry example

---
+++ 07_publishing/cutclip/README.md
@@ -0,0 +1,1 @@
+# Cutclip Readme
---
+++ 07_publishing/cutclip/cutclip.py
@@ -0,0 +1,21 @@
+import argparse
+import moviepy.editor as mp
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="CutClip", description="Cuts out a clip from a video."
+    )
+
+    parser.add_argument("-i", "--input", required=True, help="Input file")
+    parser.add_argument("-s", "--start", default=0.0, type=float, help="Start time")
+    parser.add_argument("-e", "--end", required=True, type=float, help="End time")
+    parser.add_argument(
+        "-o", "--output", required=False, default="output.mp4", help="Output file"
+    )
+
+    args = parser.parse_args()
+
+    video = mp.VideoFileClip(args.input)
+    clip = video.subclip(args.start, args.end)
+    clip.write_videofile(args.output)
